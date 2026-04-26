f6ffe3aaee6229bbab68046d424963674afff818
Katerina Ohnieva <brilliantskate2588@gmail.com>
2026-04-26 16:47:50 +0300

    Update task-8.ts

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,16 +1,17 @@
+import axios from "axios";
+
 interface Post {
 id: number;
 title: string;
 body: string;
 }
 
-
-
-import axios from "axios";
-async function fetchPosts() {
-
-const response = await axios.get<Post[]>( 'https://jsonplaceholder.typicode.com/posts'
-);
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+"https://jsonplaceholder.typicode.com/posts"
+  );
 return response.data; }
+
 fetchPosts().then((posts) => {
-console.log(posts[0].title); });
+console.log(posts[0].title); 
+});
